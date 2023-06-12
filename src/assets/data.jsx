fa5df65ef59fe15b99449b1f95bf7fabace69613
Claudinei Campos <claudineicampos95@gmail.com>
2023-06-12 10:35:06 -0300

    Reparando detalhes

--- src/assets/data.jsx
+++ src/assets/data.jsx
@@ -40,6 +40,7 @@ export const data = [
         distancia: "30 km de distância",
         preco: "R$432 diária",
         descricao: "Situado em São Paulo, o Apê Metrô Paulista oferece vista da cidade e acesso Wi-Fi gratuito, a 1,2 km do Estádio do Pacaembu e a 1,1 km do MASP São Paulo. As acomodações incluem ar condicionado, uma kitchenette totalmente equipada, uma televisão de ecrã plano e uma casa de banho privativa com um chuveiro, um secador de cabelo e produtos de higiene pessoal gratuitos. O Edifício Copan fica a 3,1 km do apartamento, enquanto a Sala São Paulo está a 3,9 km. O aeroporto mais próximo é o Aeroporto de São Paulo/Congonhas, a 12 km do Apê Metrô Paulista.",
+        tipo: "Apartamento",
         comodidades: [ "Wi-fi", "Estacionamento incluído", "Pátio ou varanda (Privativa)"]
     },
     {
@@ -49,6 +50,77 @@ export const data = [
         distancia: "88 km de distância",
         preco: "R$722 noite",
         descricao: "Numa antiga capelinha centenária e abandonada, acoplamos um antigo contêiner de transporte de cargas para criar um projeto minimalista e inusitado.Perfeita para quem busca privacidade e isolamento em meio a natureza, mas não pode abrir mão da internet, do conforto e do auto-cuidado.",
+        tipo: "Capela",
         comodidades: ["Vista para as montanhas", "Vista para o jardim", "Estacionamento incluído", "Wi-Fi", "Pátio ou varanda (Privativa)"]
+    },
+    {
+        id: uuidv4(),
+        imagem: "https://a0.muscache.com/im/pictures/miso/Hosting-43293445/original/4e891a1f-8d32-4365-8ce5-0c07a19269af.jpeg?im_w=720",
+        titulo: "Córrego do Bom Jesus, Brasil",
+        distancia: "117 km de distância",
+        preco: "R$446 noite",
+        descricao: "Entrará nossa propriedade margeado por uma charmosa área mata nativa com árvores centenárias, araucárias com suas sementes saborosas e muito ar puro, afinal são 1600m. de altitude. Estamos em uma comunidade rural, pequena e charmosa. Se você está cansado da cidade, do caos e da rotina acelerada, o CHALÉ DO IPÊ é perfeito para dar aquele respiro, aqui mais do que recarregar as energias em um fim de semana de descanso e limpeza para a mente!",
+        tipo: "Chalé",
+        comodidades: ["Vista para as montanhas", "Vista para o jardim", "Jacuzzi privativa", "Pátio ou varanda (Privativa)", "Permitido animais"]
+    },
+    {
+        id: uuidv4(),
+        imagem: "https://a0.muscache.com/im/pictures/miso/Hosting-696516059606259615/original/c1a9a68c-a533-4cb3-813f-16cf08debd36.jpeg?im_w=960",
+        titulo: "Guararema, Brasil",
+        distancia: "55 km de distância",
+        preco: "R$653 noite",
+        descricao: "Você não esquecerá o ambiente tranquilo deste destino rústico. Espaço compacto, cabana suspensa com ofurô de hidromassagem e área de fogueira. A Mata Aberta è um sítio com duas cabanas , piscina de pedra e água natural compartilhada e trilha de pedras pela mata. Vista inesquecível, totalmente solar. Check out flexível aos domingos.",
+        tipo: "Cabana",
+        comodidades: ["Vista para as montanhas", "Vista para o jardim", "Estacionamento incluído", "Câmeras de segurança na propriedade", "Jacuzzi privativa"]
+    },
+    {
+        id: uuidv4(),
+        imagem: "https://a0.muscache.com/im/pictures/miso/Hosting-26516763/original/5c75bde7-cfba-4b19-904a-300bdb5138ab.jpeg?im_w=720",
+        titulo: "Campinas, Brasil",
+        distancia: "75 km de distância",
+        preco: "R$3.779 diária",
+        descricao: "A propriedade tem estilo de uma Casa de Campo Americana no interior de São Paulo num condomínio dentro de uma Fazenda , com mais uma casa nova que disponibilizamos recentemente com total de 1.438 m2 de construção para 25 hóspedes, encontra-se dentro de um fechado rural muito seguro próximo a APA de Joaquim Egídio entre Campinas e Morungaba.",
+        tipo: "Casa",
+        comodidades: ["Estacionamento incluído", "Wi-Fi", "Câmeras de segurança na propriedade", "Jacuzzi privativa", "Pátio ou varanda (Privativa)", "Permitido animais"]
+    },
+    {
+        id: uuidv4(),
+        imagem: "https://a0.muscache.com/im/pictures/miso/Hosting-47025046/original/4d713a1e-ab4c-4d70-905f-d24b4042189f.jpeg?im_w=720",
+        titulo: "São Paulo, Brasil",
+        distancia: "40 km de distância",
+        preco: "R$167 diária",
+        descricao: "Se hospede em uma casa vegana, onde temos fábrica artesanal, loja, restaurante, spa e hospedagem. O quarto é amplo, c/ cama de solteiro nova e macia, sofá confortável, mesinha e cadeira.",
+        tipo: "Apartamento",
+        comodidades: ["Estacionamento incluído", "Wi-Fi", "Câmeras de segurança na propriedade", "Pátio ou varanda (Privativa)"]
+    },
+    {
+        id: uuidv4(),
+        imagem: "https://cf.bstatic.com/xdata/images/hotel/square200/331212420.webp?k=39506c1b5aa7a5c6c71ab1edcebeb3a5d3bea125519640060115d728c876c8bb&o=",
+        titulo: "Belo Horizonte, Brasil",
+        distancia: "150 km de distância",
+        preco: "R$259 diária",
+        descricao: "Este Hotel Gran Vivenzo Belo Horizonte dispõe de quartos com Wi-Fi gratuito. O Minas Shopping e a estação de metrô Minas Shopping estão a 12 minutos de caminhada. Todos os quartos do Hotel Gran Vivenzo Belo Horizonte dispõem de TV a cabo e frigobar. Os extras incluem ar-condicionado e banheiro privativo com secador de cabelo. Você pode usufruir da academia no local. A propriedade oferece instalações para reuniões, depósito para bagagem e lavagem a seco por um custo adicional.",
+        tipo: "Apartamento",
+        comodidades: ["Estacionamento incluído", "Wi-Fi", "Câmeras de segurança na propriedade", "Jacuzzi privativa"]
+    },
+    {
+        id: uuidv4(),
+        imagem: "https://cf.bstatic.com/xdata/images/hotel/square200/171462211.webp?k=397267cb8423472b05c46db9c48de99ca664e8b71cf9a7a8d5a2612ee33c6adf&o=",
+        titulo: "Belo Horizonte, Brasil",
+        distancia: "150 km de distância",
+        preco: "R$452 diária",
+        descricao: "Localizado em Belo Horizonte, o Normandy Hotel dispõe de um restaurante e um business center. A propriedade está situada em frente ao complexo da Igreja de São José. Os quartos do Normandy Hotel apresentam vista para a Avenida Afonso Pena, e incluem banheiro privativo, ar-condicionado e frigobar. O serviço de quarto está disponível diariamente.",
+        tipo: "Apartamento",
+        comodidades: ["Vista para o jardim", "Estacionamento incluído", "Wi-Fi", "Câmeras de segurança na propriedade", "Jacuzzi privativa", "Pátio ou varanda (Privativa)", "Lavanderia"]
+    },
+    {
+        id: uuidv4(),
+        imagem: "https://a0.muscache.com/im/pictures/miso/Hosting-746864580488340496/original/010e0024-9902-4aed-8fdc-36fae45ba2ea.jpeg?im_w=720",
+        titulo: "Atibaia, Brasil",
+        distancia: "452 km de distância",
+        preco: "R$574 noite",
+        descricao: "Você vai adorar a decoração estilosa deste charmoso lugar familiar, com restaurante, lago, trilhas e muita natureza, próximo da Pedra Grande o maior ponto turístico de Atibaia",
+        tipo: "Pousada",
+        comodidades: ["Vista para o jardim", "Estacionamento incluído", "Wi-Fi", "Pátio ou varanda (Privativa)"]
     },
 ]
